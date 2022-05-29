b8ff2eb307f8c52f1292b1f9928623789a5e3706
shubham44-code <shubham162001@gmail.com>
2022-05-29 10:55:02 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  const wave = () => {};
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">
+          I am farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
